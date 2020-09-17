d04324f92c32c4e6eba34d01f48cbbea6ef99370
Aliaksei Loi <aliaksei.loi@itechart-group.com>
2020-09-17 21:09:10 +0300

    add comments

--- index.js
+++ index.js
@@ -9,18 +9,26 @@ const countAsync = createAsync(
 );
 
 const { subscribe } = createInstance(0)
+  .add(countAsync.done, (_, value) => value)
   .add(increment, state => state + 1)
   .add(decrement, state => state - 1)
-  .add(countAsync.done, (_, value) => value)
   .reset(reset);
 
 subscribe(console.log);
 
 increment();
+// { state: 1, params: undefined }
+
 increment();
+// { state: 2, params: undefined }
+
 increment();
+// { state: 3, params: undefined }
+
 decrement();
+// { state: 2, params: undefined }
 
 (async () => {
   await countAsync(10);
+  // { state: 10, params: 10 }
 })();
